27132df08438b8e0e298555f164a0fab4c2bfeeb
deepavenkatesh <deepavenkat172@gmail.com>
2024-02-08 23:58:51 +0530

    Day - 8 Toast Notification

--- Auto Text Effect/script.js
+++ Auto Text Effect/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text = 'Welcome To The Code Zone!'
 let idx = 1
 let speed = 300 / speedEl.value
 
