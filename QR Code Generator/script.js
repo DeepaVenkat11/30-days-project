d9e715b54fc04126ac73a1ba180b64692b000ef3
deepavenkatesh <deepavenkat172@gmail.com>
2024-03-09 22:27:24 +0530

    Source link  added in QR code generator Project

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -3,23 +3,24 @@ let qr_content = document.getElementById("qrcontent");
 let qr_code = null; // Initialize qr_code to null
 
 function qrCodeGenerator(qrcontentInput) {
-    return new QRCode('qr-code', {
-        text: qrcontentInput,
-        width: 256,
-        height: 256,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H
-    });
+  return new QRCode("qr-code", {
+    text: qrcontentInput,
+    width: 256,
+    height: 256,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
 }
 
-qr_form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let qrcontentInput = qr_content.value;
-    if (qr_code === null) { // Check if qr_code is null
-        qr_code = qrCodeGenerator(qrcontentInput);
-    } else {
-        qr_code.clear(); // Clear previous code if exists
-        qr_code.makeCode(qrcontentInput);
-    }
+qr_form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let qrcontentInput = qr_content.value;
+  if (qr_code === null) {
+    // Check if qr_code is null
+    qr_code = qrCodeGenerator(qrcontentInput);
+  } else {
+    qr_code.clear();
+    qr_code.makeCode(qrcontentInput);
+  }
 });
